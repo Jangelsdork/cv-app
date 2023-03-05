9248deecf95c3c1b9e0640f771cf7b66157ac5b4
jangelsdork <j.hespe.mangelsdorf@gmail.com>
2023-03-05 16:39:58 +0100

    worked on styling, added personal info

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -3,15 +3,16 @@ export default function Resume({companyInfo, personalInfo}) {
   return (
     <div className="resumeFlow">
       <div className="resHeader">
-        <div>First Name: {personalInfo.firstName}</div>
-        <div>Last Name: {personalInfo.lastName}</div>
-        <div>Phone: {personalInfo.phoneNo} </div>
-        <div>Email: {personalInfo.email} </div>
-        <div>Address: {personalInfo.address}</div>
+        <div className="resName">{personalInfo.firstName} {personalInfo.lastName}</div>
+        <div className="resPersonal">
+          <div>☎️ {personalInfo.phoneNo} </div>
+          <div>💻  {personalInfo.email} </div>
+          <div>🏡 {personalInfo.address}</div>
+        </div>
       </div>
-      <div className="resCompany"> Company... 
-        <div>Name: {companyInfo.companyName}</div>
-        <div>City: {companyInfo.companyCity}</div>
+    <div className="resCompany">
+        <div className="resCompHeader">Previous experience</div>
+        <div className="resCompName">{companyInfo.companyName}, {companyInfo.companyCity}</div>
         <div>Date: From {companyInfo.dateFrom} Till {companyInfo.dateTo}</div>
         <div> Responsibilities: {companyInfo.companyResponsibilities}</div> 
       </div>
